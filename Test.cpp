7c0e636a3b6289567251de9420a5411a30b5f65c
AhmedSaid3617 <said.ahmed3617@gmail.com>
2023-03-13 20:41:58 +0200

    Task 1 done.

---
+++ Test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Initialize variables.
+    float r1, r2, r3, rEq, volt, current;
+    char connection;
+    // Input resistance values.
+    cout << "Circuit description: ";
+    cin >> connection >> r1 >> r2 >> r3;
+    // Input current.
+    cout << "Voltage applied: ";
+    cin >> volt;
+    // Calculate equivalent resistance.
+    if (connection == 'S')
+    {
+        rEq = r1 + r2 + r3;
+    }
+    else if (connection == 'P')
+    {
+        rEq = 1 / ((1 / r1) + (1 / r2) + (1 / r3));
+    }
+    // Calculate current.
+    current = volt / rEq;
+    // Output results.
+    cout << "Equivalent Resistance: " << rEq << endl;
+    cout << "Current: " << current << endl;
+    return 0;
+}
