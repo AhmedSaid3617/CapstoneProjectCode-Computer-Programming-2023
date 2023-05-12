4c2c92804c411f173107cc130a3958cd1052ae37
AhmedSaid3617 <said.ahmed3617@gmail.com>
2023-05-12 20:46:21 +0300

    Before Task 3

--- Task1.cpp
+++ Task1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     // Initialize variables.
-    float r1 = 5, r2, r3, rEq, volt, current;
+    float newR, rEq, volt, current;
     char connection;
     string circuit;
     int breakPoint = 1;
@@ -25,7 +25,8 @@ int main(){
     }
     else {
         // Get first resistance.
-        // This loop finds the second space character (breakpoint). First resistance is between index 1 and breakpoint.
+        // This loop finds the second space character (breakpoint). 
+        // First resistance is between index 1 and breakpoint.
         for (int i=2; i < circuit.length(); i++){
             if (circuit[i] == ' ') {
                 rEq = stof(circuit.substr(1, i-1));
@@ -34,17 +35,18 @@ int main(){
             }
         }
 
-        // Loops over input from breakpoint, resistance is between breakpoint and next space. Adds resistance to rEq.
+        // Loops over input from breakpoint, resistance is between breakpoint and next space. 
+        // Adds resistance to rEq.
 
         // Calculate equivalent series resistance.
         if (connection == 'S'){
             for (int i=breakPoint + 1; i < circuit.length(); i++){
                 if (circuit[i] == ' ') {
                     // Resistance is between breakpoint and space.
-                    r1 = stof(circuit.substr(breakPoint, i-breakPoint));
+                    newR = stof(circuit.substr(breakPoint, i-breakPoint));
 
                     // Add resistance to rEq.
-                    rEq += r1;
+                    rEq += newR;
 
                     // Set breakpoint to new space character index.
                     breakPoint = i;
@@ -56,10 +58,10 @@ int main(){
         else {
             for (int i=breakPoint + 1; i < circuit.length(); i++){
                 if (circuit[i] == ' ') {
-                    r1 = stof(circuit.substr(breakPoint, i-breakPoint));
+                    newR = stof(circuit.substr(breakPoint, i-breakPoint));
 
                     // Calculates rEq parallel with new resistance.
-                    rEq = 1/(1/rEq + 1/r1);
+                    rEq = 1/(1/rEq + 1/newR);
 
                     breakPoint = i;
                 }
